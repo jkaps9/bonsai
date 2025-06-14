1e95b97180dd3bd0ec9ab23b5675ca337e6abea8
Josh <15219491+jkaps9@users.noreply.github.com>
2025-06-14 01:38:47 -0400

    feat: style mobile menu part 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles.css";
+import hamburgerImg from "./images/hamburger.svg";
+import closeImg from "./images/close.svg";
+
+const menuIcon = document.querySelector("#hamburger");
+let menuOpen = false;
+const menu = document.querySelector("nav");
+
+menuIcon.addEventListener("click", () => {
+  menuOpen = !menuOpen;
+  if (menuOpen) {
+    menuIcon.src = closeImg;
+  } else {
+    menuIcon.src = hamburgerImg;
+  }
+});
