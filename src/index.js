4e0d61859d3d3e5fd9391b498c166695473d39c8
Josh <15219491+jkaps9@users.noreply.github.com>
2025-07-18 00:03:00 -0400

    add functionality to toggle ball using javascript

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import hamburgerImg from "./images/hamburger.svg";
 import closeImg from "./images/close.svg";
 
+// MOBILE MENU
 const menuIcon = document.querySelector("#hamburger");
 let menuOpen = false;
 const menu = document.querySelector("nav");
@@ -14,5 +15,28 @@ menuIcon.addEventListener("click", () => {
   } else {
     menuIcon.src = hamburgerImg;
     menu.classList.remove("visible");
+  }
+});
+
+// TOGGLE
+const toggle = document.querySelector(".toggle");
+const toggleBall = toggle.querySelector(".toggle-ball");
+const monthly = document.querySelector(".monthly-text");
+const yearly = document.querySelector(".yearly-text");
+let pricing = "monthly";
+
+toggle.addEventListener("click", () => {
+  if (pricing === "monthly") {
+    // Move to yearly
+    pricing = "yearly";
+    monthly.style = "opacity: 0.65;";
+    yearly.style = "opacity: 1;";
+    toggleBall.style = "transform: translateX(21px);";
+  } else {
+    // Move to monthly
+    pricing = "monthly";
+    monthly.style = "opacity: 1;";
+    yearly.style = "opacity: 0.65;";
+    toggleBall.style = "transform: translateX(0);";
   }
 });
