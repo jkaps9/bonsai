9ba804143754c7095a21c2494237b7bf8aa804df
jkaps-p6p <15219491+jkaps9@users.noreply.github.com>
2025-07-23 18:12:41 -0400

    toggle changes price on cards

--- src/index.js
+++ src/index.js
@@ -23,10 +23,15 @@ const toggle = document.querySelector(".toggle");
 const toggleBall = toggle.querySelector(".toggle-ball");
 const monthly = document.querySelector(".monthly-text");
 const yearly = document.querySelector(".yearly-text");
-const price-boxes = document.querySelectorAll(".price-box");
-const prices = [{pricing: "monthly", prices: [24,39,79]}, {pricing: "yearly", prices: [17,32,52]};
+const priceBoxes = document.querySelectorAll(".price-box");
+const prices = [
+	{priceTime: "monthly", prices: [24,39,79]}, 
+	{priceTime: "yearly", prices: [17,32,52]}
+];
 let pricing = "monthly";
-
+//const header = document.querySelector(".section-header");
+//const errorDiv = document.createElement("div");
+//header.appendChild(errorDiv);
 toggle.addEventListener("click", () => {
   if (pricing === "monthly") {
     // Move to yearly
@@ -34,12 +39,32 @@ toggle.addEventListener("click", () => {
     monthly.style = "opacity: 0.65;";
     yearly.style = "opacity: 1;";
     toggleBall.style = "transform: translateX(21px);";
-	  price-boxes[0]
-  } else {
+	  
+
+	  //errorDiv.innerHTML=pricing;
+	  //errorDiv.innerHTML+=JSON.stringify(priceList);
+	 // header.appendChild(errorDiv);
+	    } else {
     // Move to monthly
     pricing = "monthly";
     monthly.style = "opacity: 1;";
     yearly.style = "opacity: 0.65;";
     toggleBall.style = "transform: translateX(0);";
   }
+
+	const priceList = prices
+		.filter(item => item.priceTime===pricing)[0]
+		.prices;
+	  for(let i=0; i<3; i++){
+		  const price = priceBoxes[i].querySelector(".price>.amount");
+		  if (pricing === "yearly"){
+			  const billedYearly = document.createElement("p");
+			  billedYearly.innerHTML = "Billed yearly";
+			  billedYearly.style="justify-self: flex-end; font-size:1rem;";
+			  billedYearly.classList.add("billedYearly");
+
+			  priceBoxes[i].appendChild(billedYearly);	
+		  }
+	 	 price.innerHTML = priceList[i];
+	  }
 });
